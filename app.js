099bd3d3865357594caf99886944c90a4cb39ca7
unknown <jamesmkubwa18@gmail.com>
2023-10-30 17:45:40 +0300

    use javascript to give use to buttons
    user is able to make choice rock,paper,scissors but clicking button...same wth the computer

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 
 // DOM caching
+let userScore= 0;
+let computerScore =0;
 
 const announceWin_h1 = document.querySelector('#announce-won');
 const trackPoints_p = document.querySelector('#track-points');
@@ -8,6 +10,7 @@ const playerScore_span = document.querySelector('#player-score');
 const compChoice_div = document.querySelector('#comp-choice');
 const computerScore_span = document.querySelector('#computer-score');
 const choice = document.querySelectorAll('#choices div')
+const start = document.querySelector('#start')
 const rock = document.querySelector('#r');
 const paper = document.querySelector('#p')
 const scissors = document.querySelector('#s')
@@ -16,22 +19,131 @@ const scissors = document.querySelector('#s')
 // get user choice
 
 Array.from(choice).forEach(btn =>{
-  btn.addEventListener('click', userChoice)
+  btn.addEventListener('click', function(event){
+   userChoice(event);
+   event.target.style.border = '2px solid black';
+  setTimeout(function(){
+    event.target.style.border = '';
+  },100);
+
+  start.style.display = 'none';
+  userChoice_div.style.display = 'block';
+  compChoice_div.style.display = 'block';
+
+
+  })
 })
 
-function userChoice(btn){
-  return btn.target.id;
+// if user wins
+function win(){
+  userScore++;
+  playerScore_span.innerHTML = `Player Score: ${userScore}`
+  userChoice_div.style.backgroundColor = "green";
+  compChoice_div.style.backgroundColor = "red";
+  setTimeout(function(){
+    userChoice_div.style.backgroundColor = "";
+    compChoice_div.style.backgroundColor = "";
+  },1000)
+  
+
 }
+
+// if user loses to comp
+
+function lose(){
+  computerScore++;
+  computerScore_span.innerHTML = `Computer Score: ${computerScore}`
+  compChoice_div.style.backgroundColor = "green";
+  userChoice_div.style.backgroundColor = "red";
+  setTimeout(function(){
+    userChoice_div.style.backgroundColor = "";
+    compChoice_div.style.backgroundColor = "";
+  },1000)
+
+}
+
+//draw
+function draw(){
+  compChoice_div.style.backgroundColor = "grey";
+  userChoice_div.style.backgroundColor = "grey";
+  setTimeout(function(){
+    userChoice_div.style.backgroundColor = "";
+    compChoice_div.style.backgroundColor = "";
+  },1000)
+
+}
+
+function renderPlayerSelection(playerSelection){
+  if(playerSelection == "r" ){
+    userChoice_div.className = 'rock'
+  }else if(playerSelection == "p"){
+    userChoice_div.className = 'paper'
+  }else{
+    userChoice_div.className ='scissors'
+  }
+}
+
+
+function renderComputerSelection(computerSelection){
+  if(computerSelection == "r"){
+    compChoice_div.className = 'rock'
+  }else if(computerSelection == "p"){
+    compChoice_div.className = 'paper'
+  }else{
+    compChoice_div.className ='scissors'
+  }
+}
+
+
+// determine user win, computer win or draw
+function game(playerSelection){
+const computerSelection = computerChoice();
+renderPlayerSelection(playerSelection);
+renderComputerSelection(computerSelection);
+
+   switch(playerSelection + computerSelection){
+    case "pr":
+    case "sp":
+    case "rs":
+            win();
+    break;
+
+    case "rp":
+    case "ps":
+    case "sr":
+           lose();
+      break;
+
+    case "rr":
+    case "pp":
+    case "ss":
+           draw();
+      break;
+   }
+
+  
+}
+
+//get user choice
+function userChoice(event){
+  game(event.target.id);
+}
+
 
 //get computer choice
 
 function computerChoice(){
   const choices = [rock,paper,scissors];
   const randomComputerChoice = choices[Math.floor(Math.random()*choices.length)];
-  console.log(randomComputerChoice.id);
-
+  return randomComputerChoice.id;
+ 
 }
-computerChoice();
+
+
+
+
+// check winner
+
 
 
 
